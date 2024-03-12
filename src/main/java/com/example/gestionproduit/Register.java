c8450d10770687d7114f1bbb4a25e2bab994959c
sembene <sembenpape4@gmail.com>
2024-03-12 09:26:24 +0000

    link inscription vers login

--- src/main/java/com/example/gestionproduit/Register.java
+++ src/main/java/com/example/gestionproduit/Register.java
@@ -2,8 +2,15 @@ package com.example.gestionproduit;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class Register {
 
@@ -22,8 +29,12 @@ public class Register {
     }
 
     @FXML
-    void btn_login(ActionEvent event) {
-
+    void btn_login(ActionEvent event)  throws IOException {
+        Parent signin = FXMLLoader.load(getClass().getResource("hello-view.fxml"));
+        Scene scene = new Scene(signin,700, 500);
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
     }
 
 }
