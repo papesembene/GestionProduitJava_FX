343a2c76dcf06a59d3add280edbd5eaf184dfee7
sembene <sembenpape4@gmail.com>
2024-03-12 09:36:19 +0000

    implementation du code de connexion avec pgsql

--- src/main/java/com/example/gestionproduit/model/Db.java
+++ src/main/java/com/example/gestionproduit/model/Db.java
@@ -1,4 +1,29 @@
 package com.example.gestionproduit.model;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class Db {
+    private final String server = "localhost";
+    private final String username = "postgres";
+    private final String password = "passer";
+    private final String bd = "gestion_produits_fx";
+    private final String url = "jdbc:postgresql://localhost:5432/gestion_produits_fx";
+    private Connection conn;
+
+    public Db() {
+    }
+
+    public Connection getConnection() {
+        try {
+            Class.forName("org.postgresql.Driver");
+            this.conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/gestion_vehicules", "postgres", "passer");
+            //System.out.println("Connection a la base reussi ");
+        } catch (Exception var2) {
+            this.conn = null;
+            System.out.print("Erreur de Connection  ");
+        }
+
+        return this.conn;
+    }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.example.gestionproduit {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
 
 
     opens com.example.gestionproduit to javafx.fxml;
