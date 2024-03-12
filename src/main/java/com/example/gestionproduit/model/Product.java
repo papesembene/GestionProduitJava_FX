8976e566eda8786d08897dc455f1f4e0797db65f
sembene <sembenpape4@gmail.com>
2024-03-12 11:38:19 +0000

    update product.java

--- src/main/java/com/example/gestionproduit/model/Product.java
+++ src/main/java/com/example/gestionproduit/model/Product.java
@@ -1,4 +1,73 @@
 package com.example.gestionproduit.model;
 
 public class Product {
+    private int id;
+    private int category_id;
+
+    private  String name;
+
+    private  int price;
+    private  int quantity;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getCategory_id() {
+        return category_id;
+    }
+
+    public void setCategory_id(int category_id) {
+        this.category_id = category_id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public Product(int id, int category_id, String name, int price, int quantity) {
+        this.id = id;
+        this.category_id = category_id;
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+    public Product() {
+
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", category_id=" + category_id +
+                ", name='" + name + '\'' +
+                ", price=" + price +
+                ", quantity=" + quantity +
+                '}';
+    }
 }
