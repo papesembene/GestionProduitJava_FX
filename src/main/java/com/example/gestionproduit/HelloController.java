a91696c0d812935056d725822a0bda4028022fcd
sembene <sembenpape4@gmail.com>
2024-03-12 09:15:10 +0000

    update HelloController ,HelloApplication , hello-view and register

--- src/main/java/com/example/gestionproduit/HelloController.java
+++ src/main/java/com/example/gestionproduit/HelloController.java
@@ -1,10 +1,17 @@
 package com.example.gestionproduit;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class HelloController {
     @FXML
@@ -16,6 +23,15 @@ public class HelloController {
     @FXML
     void btnLogin(ActionEvent event) {
 
+    }
+
+    @FXML
+    void bntInscrire(ActionEvent event) throws IOException {
+        Parent signin = FXMLLoader.load(getClass().getResource("/com/example/gestionprojet/register.fxml"));
+        Scene scene = new Scene(signin);
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
     }
 
 }
