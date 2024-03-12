2ed7261993e12a6356071234cf5cf6a71f039959
sembene <sembenpape4@gmail.com>
2024-03-12 19:55:51 +0000

    maj ProductController

--- src/main/java/com/example/gestionproduit/ProductController.java
+++ src/main/java/com/example/gestionproduit/ProductController.java
@@ -1,4 +1,66 @@
 package com.example.gestionproduit;
 
+import com.example.gestionproduit.model.Category;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
 public class ProductController {
+
+    @FXML
+    private ComboBox<Category> categoryCombo;
+
+    @FXML
+    private TableColumn<?, ?> colCategory;
+
+    @FXML
+    private TableColumn<?, ?> colId;
+
+    @FXML
+    private TableColumn<?, ?> colName;
+
+    @FXML
+    private TableColumn<?, ?> colPrice;
+
+    @FXML
+    private TableColumn<?, ?> colQte;
+
+    @FXML
+    private TextField nameInput;
+
+    @FXML
+    private TextField nameInput1;
+
+    @FXML
+    private TextField priceInput;
+
+    @FXML
+    private TableView<?> productTable;
+
+    @FXML
+    private TextField qteInput;
+
+    @FXML
+    void btnAdd(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnDelete(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnEdit(ActionEvent event) {
+
+    }
+
+    @FXML
+    void searchInput(ActionEvent event) {
+
+    }
+
 }
