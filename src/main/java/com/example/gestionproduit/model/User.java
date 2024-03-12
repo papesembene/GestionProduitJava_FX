7a881a8cdf09e05332d4247060a51668e61e0491
sembene <sembenpape4@gmail.com>
2024-03-12 11:43:18 +0000

    maj User.java

--- src/main/java/com/example/gestionproduit/model/User.java
+++ src/main/java/com/example/gestionproduit/model/User.java
@@ -1,4 +1,93 @@
 package com.example.gestionproduit.model;
 
 public class User {
+    private int id;
+    private String f_name;
+    private String l_name;
+    private String email;
+    private String password;
+    private String phone;
+    private String login;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getF_name() {
+        return f_name;
+    }
+
+    public void setF_name(String f_name) {
+        this.f_name = f_name;
+    }
+
+    public String getL_name() {
+        return l_name;
+    }
+
+    public void setL_name(String l_name) {
+        this.l_name = l_name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public User(String f_name, String l_name, String email, String password, String phone, String login) {
+
+        this.f_name = f_name;
+        this.l_name = l_name;
+        this.email = email;
+        this.password = password;
+        this.phone = phone;
+        this.login = login;
+    }
+    public User() {
+
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", f_name='" + f_name + '\'' +
+                ", l_name='" + l_name + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", phone='" + phone + '\'' +
+                ", login='" + login + '\'' +
+                '}';
+    }
 }
