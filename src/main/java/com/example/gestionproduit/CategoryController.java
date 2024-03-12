28d2831dd27e160b32d56a24b9bcfe03da9b71a6
sembene <sembenpape4@gmail.com>
2024-03-12 20:20:54 +0000

    modification sur CategoryController

--- src/main/java/com/example/gestionproduit/CategoryController.java
+++ src/main/java/com/example/gestionproduit/CategoryController.java
@@ -1,4 +1,46 @@
 package com.example.gestionproduit;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
 public class CategoryController {
+
+    @FXML
+    private TableView<?> categoryTable;
+
+    @FXML
+    private TableColumn<?, ?> colId;
+
+    @FXML
+    private TableColumn<?, ?> colName;
+
+    @FXML
+    private TextField nameInput;
+
+    @FXML
+    private TextField searchInput;
+
+    @FXML
+    void btnAdd(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnDelete(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnEdit(ActionEvent event) {
+
+    }
+
+    @FXML
+    void searchInput(ActionEvent event) {
+
+    }
+
 }
