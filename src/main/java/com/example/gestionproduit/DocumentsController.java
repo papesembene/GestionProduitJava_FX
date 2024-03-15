6f51a289f6e9a98b614b128ba88d372e9be09059
sembene <sembenpape4@gmail.com>
2024-03-15 15:01:51 +0000

    maj documents.fxml

--- src/main/java/com/example/gestionproduit/DocumentsController.java
+++ src/main/java/com/example/gestionproduit/DocumentsController.java
@@ -1,4 +1,18 @@
 package com.example.gestionproduit;
+git
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 
 public class DocumentsController {
+
+    @FXML
+    void btnexcel(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnpdf(ActionEvent event) {
+
+    }
+
 }
