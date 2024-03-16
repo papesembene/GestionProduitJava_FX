d30468b176f3f65af6fdbabe2ae3dff888ed57ac
sembene <sembenpape4@gmail.com>
2024-03-16 13:04:29 +0000

    gerer la taille fixe et centrer la page ainsi que le background-color

--- src/main/java/com/example/gestionproduit/HelloApplication.java
+++ src/main/java/com/example/gestionproduit/HelloApplication.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
 
@@ -13,8 +14,11 @@ public class HelloApplication extends Application {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Login");
-        stage.setResizable(false);
         stage.setScene(scene);
+        stage.setResizable(false);
+        stage.initStyle(StageStyle.UNDECORATED);
+        stage.sizeToScene();
+        stage.centerOnScreen();
         stage.show();
     }
     public static void main(String[] args) {
